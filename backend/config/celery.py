e26d77d6c7c839ac89c33f994fa915a7b560ea0f
MHM-Rahmati <131310803+MHM-Rahmati@users.noreply.github.com>
2026-09-08 20:39:22 +0330

    Add explanatory comments to celery.py for clarity

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -4,6 +4,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
 
+# Create a Celery application instance named 'renting'.
 app = Celery('renting')
 
 # Using a string here means the worker doesn't have to serialize
@@ -15,4 +16,4 @@ app.autodiscover_tasks()
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
-    print(f'Request: {self.request!r}') 
+    print(f'Request: {self.request!r}')
